3fcc268b2ef8f2fe2c5daed5bcc0da8ddd2929a7
Ilias Kompogiannis <ilk@tractive.com>
2017-11-15 20:06:10 +0100

    Init commit

---
+++ src/main/java/com/iliaskomp/filmsarecool/ui/home/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iliaskomp.filmsarecool.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iliaskomp.filmsarecool.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+}
